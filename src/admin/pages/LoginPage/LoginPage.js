2c50d47b0e4bc34c11ba0f2bc1a3feae47b55b5a
Anurag <anurag852001@gmail.com>
2025-05-18 18:41:18 +0530

    Login page

--- src/admin/pages/LoginPage/LoginPage.js
+++ src/admin/pages/LoginPage/LoginPage.js
@@ -1,5 +1,34 @@
+import { color } from 'echarts';
+import WelcomeBackGround from '../../../components/WelcomeBackground/WelcomeBackGround';
 import styles from './LoginPage.module.css';
+import { useState } from 'react';
 function LoginPage(){
-    return <div className={styles.mainContainer}>This is the login page</div>
+    const [isAdmin,setIsAdmin] = useState(false);
+
+    const onAdminSwitchToggleClick=()=>{
+        setIsAdmin(!isAdmin);
+    }
+
+    return <WelcomeBackGround  >
+        <div className={styles.mainContainer}>
+        <div className={styles.leftSection}>Welcome to login</div>
+        <div className={styles.rightSection}> 
+            <div className={styles.loginPortal}> 
+                <div className={`${styles.adminWebsiteSwitch} ${!isAdmin ? styles.websiteActive : ''}`} onClick={onAdminSwitchToggleClick}> 
+                    
+                    <span className ={ isAdmin ? styles.adminSwitchSpanActive:styles.adminSwitchSpanDisabled }>Admin</span>
+                    <span className={!isAdmin ? styles.adminSwitchSpanActive:styles.adminSwitchSpanDisabled}>User</span>
+                    <div className={styles.slider}></div>
+                </div>
+            <div className={styles.inputSection}>
+                <input type='Text' placeholder='Enter email or phone number'></input>
+                <input type='Password' placeholder='Enter password'></input>
+                <button>Login</button>
+            </div>
+
+            </div>
+        </div>
+        </div>
+        </WelcomeBackGround>
 }
 export default LoginPage;
